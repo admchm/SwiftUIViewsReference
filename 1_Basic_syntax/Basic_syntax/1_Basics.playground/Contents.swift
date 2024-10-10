daf54bd9bcc9389765e162b42d80952eab2c3ff5
Adam Chomicki <admchm@outlook.com>
2024-10-10 12:40:06 +0200

    Create project for basics info about SwiftUI. Create first playground

---
+++ 1_Basic_syntax/Basic_syntax/1_Basics.playground/Contents.swift
@@ -0,0 +1,11 @@
+// Basics:
+
+// - In SwiftUI, you build a UI with Views and then you change those views with Modifiers.
+// - Views can be organized in containers. Some containers organize views in one direction. This is called Stack.
+//
+// We have stacks like:
+// - Vertical Stack or as SwiftUI calls it, a “VStack”.
+// - There is another stack that can organize views horizontally. SwiftUI calls this horizontal stack an HStack.
+// - Another stack view will organize your views so they are one on top of another. This is called the Depth Stack or ZStack.
+
+// It is beneficial to know that Apple refers to child views that have no children of their own as “leaf views”.

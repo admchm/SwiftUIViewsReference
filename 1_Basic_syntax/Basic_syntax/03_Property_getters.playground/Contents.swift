5c1227ff8604de1198cec97de3bb2d9246e7d048
Adam Chomicki <admchm@outlook.com>
2024-10-10 13:28:45 +0200

    Add playgrounds for property getters and opaque types

---
+++ 1_Basic_syntax/Basic_syntax/03_Property_getters.playground/Contents.swift
@@ -0,0 +1,11 @@
+// SwiftUI Property getters:
+
+import SwiftUI
+
+struct BasicSyntax: View {
+    var body: some View {
+        get {
+            return Text("Hello World!")
+        }
+    }
+}
---
+++ 1_Basic_syntax/Basic_syntax/04_Opaque_types.playground/Contents.swift
@@ -0,0 +1,5 @@
+// Opaque types:
+
+// The keyword some is specifying that it is an opaque type (“opaque”, when referring to languages, means “hard or impossible to understand.”)
+// In the code example, it is “hard or impossible to understand” which type of view is being returned but at least you know it is a view.
+// In this case, it is OK with just knowing that some View is being returned and can use that view to draw the screen.

8f250e055d65b3e1badf333887033887994f4078
Adam Chomicki <admchm@outlook.com>
2024-10-14 14:33:57 +0200

    Create separate project for Layout behavior

---
+++ 03_Layout_Behavior/03_Layout_Behavior/03_Layout_BehaviorApp.swift
@@ -0,0 +1,20 @@
+//
+//  03_Layout_BehaviorApp.swift
+//
+//  Created by Adam Chomicki on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Layout_BehaviorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ScrollView {
+                VStack {
+                    
+                }
+            }
+        }
+    }
+}

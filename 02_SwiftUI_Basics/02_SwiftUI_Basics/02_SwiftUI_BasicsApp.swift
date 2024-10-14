2427cd1d875f07fc5f02f3ab42fee1b09a7c5e35
Adam Chomicki <admchm@outlook.com>
2024-10-14 13:41:47 +0200

    Create view hierarchy in single project and add scroll view

---
+++ 02_SwiftUI_Basics/02_SwiftUI_Basics/02_SwiftUI_BasicsApp.swift
@@ -0,0 +1,23 @@
+//
+//  02_SwiftUI_BasicsApp.swift
+//
+//  Created by Adam Chomicki on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ScrollView {
+                VStack {
+                    SFSymbols()
+                        .padding()
+                    Layers()
+                        .padding()
+                }
+            }
+        }
+    }
+}
---
+++ 02_SwiftUI_Basics/02_SwiftUI_Basics/Layers.swift
@@ -0,0 +1,38 @@
+//
+//  Layers.swift
+//  Template
+//
+//  Created by Adam Chomicki on 10/10/2024.
+//
+
+import SwiftUI
+
+struct Layers: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Text("Images")
+                .font(.largeTitle)    // this modifier overrides the one from parent
+            
+            Text("Using SF Symbols")
+                .foregroundColor(Color.gray)
+            
+            Text("You can put common modifiers on parent views to be applied to all the child views.")
+                .frame(maxWidth: .infinity)
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color.blue)
+            
+            // use "systemName" when you want to use "SF Symbols"
+            Image(systemName: "hand.thumbsup.fill")
+                .font(.largeTitle)
+            
+            // Image("SF Symbols") - regular image from Assets.xcassets
+        }
+        .font(.title)
+        .ignoresSafeArea(edges: .bottom) // ignore the bottom screen border
+    }
+}
+
+#Preview {
+    Layers()
+}
